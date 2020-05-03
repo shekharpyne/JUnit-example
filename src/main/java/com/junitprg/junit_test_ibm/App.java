4e0cf6a84dc206305084d6f4a439063ab6be42df
Shekhar Pyne <shekpyne@in.ibm.com>
2020-05-04 01:33:23 +0530

    New

---
+++ src/main/java/com/junitprg/junit_test_ibm/App.java
@@ -0,0 +1,17 @@
+package com.junitprg.junit_test_ibm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public int add(int x, int y) {
+    	return x+y;
+    }
+}
---
+++ src/test/java/com/junitprg/junit_test_ibm/AppTest.java
@@ -0,0 +1,14 @@
+package com.junitprg.junit_test_ibm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+		assertEquals(10, 10);
+	}
+
+}
